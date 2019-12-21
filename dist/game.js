308ae0e46c2b9927031f2a7b8f4e6cfc115dfb00
Lin-H <lh04667649@gmail.com>
2019-12-21 17:32:17 +0800

    add pipes

--- dist/game.js
+++ dist/game.js
@@ -12,6 +12,7 @@ var MyGame = (function () {
       preload() {
           this.load.image('ground', 'assets/ground.png');
           this.load.image('background', 'assets/background.png');
+          this.load.image('pipe', 'assets/pipe.png');
           this.load.spritesheet('bird', 'assets/bird.png', {
               frameWidth: 92,
               frameHeight: 64,
@@ -27,7 +28,9 @@ var MyGame = (function () {
           for (let i = 0; i < Math.ceil(WIDTH / 36); i++) {
               platforms.create(16 + 36 * i, 832, 'ground');
           }
+          platforms.setDepth(10);
           this.bird = this.physics.add.sprite(0, 0, 'bird');
+          this.bird.setDepth(2);
           this.bird.setCollideWorldBounds(true);
           this.physics.add.collider(this.bird, platforms);
           this.anims.create({
@@ -57,10 +60,24 @@ var MyGame = (function () {
           });
           this.bird.play('birdfly');
           this.input.on('pointerdown', this.fly, this);
+          this.makePipes();
       }
       update() {
       }
-      start() { }
+      start() {
+      }
+      makePipes() {
+          let up = this.physics.add.image(1400, 300, 'pipe');
+          up.setFlipY(true);
+          let down = this.physics.add.image(400, 700, 'pipe');
+          up.setGravityY(-2700); // 反重力
+          down.setGravityY(-2700);
+          down.setVelocityX(-200);
+          up.setVelocityX(-200);
+          this.physics.collide(this.bird, down, () => {
+              console.log(111);
+          });
+      }
       fly() {
           this.bird.setAngle(-25);
           this.birdTween.resume();
